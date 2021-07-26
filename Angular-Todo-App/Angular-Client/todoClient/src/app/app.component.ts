f526b6b7be400f10b33c39ce5c9fe553400cf3f5
invisiblegeek1 <appumoorthy15@gmail.com>
2021-07-26 15:34:59 +0530

    initial commit of e-commerce

--- Angular-Todo-App/Angular-Client/todoClient/src/app/app.component.ts
+++ Angular-Todo-App/Angular-Client/todoClient/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoClient';
+
+  items=[
+    {
+      title:"laptop",
+      price:2000,
+      imageUrl:"assets/opencart.svg",
+
+    },
+    {
+      title:"mobile",
+      price:3000,
+      imageUrl:"assets/opencart.svg",
+
+    },
+    {
+      title:"ipod",
+      price:1000,
+      imageUrl:"assets/opencart.svg",
+
+    },
+    {
+      title:"pendrive",
+      price:1000,
+      imageUrl:"assets/opencart.svg",
+
+    }
+  ]
 }
---
+++ Angular-Todo-App/Angular-Client/todoClient/src/app/item/item.component.ts
@@ -0,0 +1,14 @@
+import { Component,Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent  {
+  @Input("value")
+  item:any={};
+
+  
+
+}
