0b12227b5bb69c6b92046310b2a67ecccfd38163
pushparaja <appumoorthy15@gmail.com>
2021-07-28 14:57:39 +0000

    modal and form added

--- Angular-Todo-App/Angular-Client/todoClient/src/app/item/item.component.ts
+++ Angular-Todo-App/Angular-Client/todoClient/src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component,Input, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-item',
@@ -8,6 +9,27 @@ import { Component,Input, OnInit } from '@angular/core';
 export class ItemComponent  {
   @Input("value")
   item:any={};
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) {}
+
+  open(popContent:any) {
+    this.modalService.open(popContent, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   
 
